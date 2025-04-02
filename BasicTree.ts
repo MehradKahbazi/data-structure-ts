6c9c48a72c8508e75a1e9aa37c4851dca87d8060
unknown <mehradhetfield@gmail.com>
2025-04-02 06:51:34 +0330

    fix: tree refactor

--- BasicTree.ts
+++ BasicTree.ts
@@ -8,13 +8,32 @@ type NodeType ={
 }
 
 class TreeNode {
-    public children: NodeType[] = [];
-    constructor(public value , public parent: null | TreeNode = null ){}
+    public children: any[] = [];
+    constructor(public value:string , public parent: null | TreeNode = null ){}
 
     addNode(value: string){
-        const node = new TreeNode(value, this)
-        this.children.push(node)
-        return {node, index: this.children.length -1};
+        const segments = value.split('/');
+
+        if(segments.length === 0){
+            return
+        }
+
+        if(segments.length === 1){
+            const node = new TreeNode(segments[0], this);
+            this.children.push(node)
+            return {node, index: this.children.length -1};
+        }
+
+        const existingChildNode = this.children.find(child => child.value === segments[0])
+        if(existingChildNode){
+            existingChildNode.addNode(segments.slice(1).join('/'));
+        }else{
+            const node = new TreeNode(segments[0], this);
+            node.addNode(segments.slice(1).join('/'));
+            this.children.push(node);
+            return {node, index: this.children.length -1};
+        }
+
     }
 
     removeNode(index: number){
@@ -23,16 +42,26 @@ class TreeNode {
 }
 
 class Tree{
-    public root:NodeType;
-    constructor(public rootValue){
+    public root:any;
+    constructor(public rootValue: string){
         this.root = new TreeNode(rootValue)
     }
+    add(path: string){
+        this.root.addNode(path)
+    }
+
+    remove(path: string){}
 }
 
 const fileSystem = new Tree('/');
-const documentsNodeData = fileSystem.root.addNode('/documents');
-const gamesNodeData = fileSystem.root.addNode('/games');
+const documentsNodeData = fileSystem.root.addNode('documents');
+const gamesNodeData = fileSystem.root.addNode('games');
 documentsNodeData.node.addNode('results.txt')
 gamesNodeData.node.addNode('code.exe');
+
+fileSystem.add('documents/personal/tex.docx')
+fileSystem.add('games/cod.exe')
+fileSystem.add('games/cod2.exe')
+fileSystem.remove('games/cod2.exe')
 
 console.log(fileSystem);
